a752e49602d71e9a61f7f2fc50ed7e818d107040
Jasper N. Brouwer <jasper@nerdsweide.nl>
2016-08-25 17:21:42 +0200

    Wire Game repository in DI container

--- api/app/app.php
+++ api/app/app.php
@@ -4,6 +4,7 @@ require __DIR__ . '/bootstrap.php';
 // init Silex app
 $app = new Silex\Application();
 
+require_once __DIR__ . '/services.php';
 require_once __DIR__ . '/routing.php';
 
 return $app;
