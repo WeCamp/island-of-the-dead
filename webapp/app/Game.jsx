3b0e51b7cbd6e7bba5a74c5b66f01b5b53740ec8
Joralf Quist <j.quist@meteovista.nl>
2016-08-26 18:38:02 +0200

    intermediate commit where geolocation is updated every 5 seconds

--- webapp/app/Game.jsx
+++ webapp/app/Game.jsx
@@ -7,24 +7,44 @@ export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      latitude: null,
-      longitude: null,
-      state: null,
-      gameId: null,
+      latitude: 52.3725,
+      longitude: 5.634,
+      status: null,
+      gameId: 1,
+      objectsInGame: null
     }
+    this.timeout;
   }
   displayStateInConsole() {
-    console.log("gameState:", this.state.state, "gameId:", this.state.gameId, "latitude:", this.state.latitude, "longitude:", this.state.longitude);
+    console.log("gameState:", this.state.status, "gameId:", this.state.gameId, "latitude:", this.state.latitude, "longitude:", this.state.longitude);
   }
   componentDidMount() {
     console.log("componentDidMount");
+    this.startGame();
+    this.getGeoLocationAndUpdateMap();
+  }
+  startGame() {
+    var formData = new FormData();
+    formData.set("latitude", this.props.latitude);
+    formData.set("longitude", this.props.longitude);
 
-    // yes, this is ugly :)
-    var that = this;
+    var myInit = { method: 'POST',
+             body: formData };
+    fetch('http://islandofthedead.com/game/start', myInit)
+      .then(function(response) {
+        return response.json();
+      })
+      .then((jsonResponse) => {
+        this.setState({
+          gameId: jsonResponse.gameId,
+        });
+      })
+  }
+  getGeoLocationAndUpdateMap() {
     var lat;
     var lon;
 
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition((position) => {
       lat = position.coords.latitude;
       lon = position.coords.longitude;
 
@@ -35,32 +55,45 @@ export default class Game extends React.Component {
       var myInit = { method: 'POST',
                body: formData };
 
-      fetch('http://islandofthedead.com/game/start', myInit)
+      fetch(`http://islandofthedead.com/player/move/${this.state.gameId}`, myInit)
         .then(function(response) {
+          console.log("fetching data from backend");
           return response.json();
         })
-        .then(function(jsonResponse) {
-          that.setState({
-            state: jsonResponse.state,
-            gameId: jsonResponse.gameId,
+        .then((jsonResponse) => {
+          this.setState({
+            status: jsonResponse.state,
             latitude: lat,
             longitude: lon,
+            objectsInGame: jsonResponse.occupants
           });
+        }).then(() => {
+          this.timeout = setTimeout(() => {
+            this.getGeoLocationAndUpdateMap();
+          }, 5000);
         });
     });
-
+  }
+  componentWillUnmount() {
+    clearTimeout(this.timeout);
   }
 
-  componentWillUpdate() {
-
-  }
   render() {
-    {this.displayStateInConsole()}
-    const coordinates = {lat: this.state.latitude, lng: this.state.longitude}
     console.log("rendering component");
+    {this.displayStateInConsole()}
+    if (this.state.status === null) {
+      return(
+        <div>
+          Game Loading ...
+        </div>
+      );
+    }
+
+    const coordinates = {lat: this.state.latitude, lng: this.state.longitude}
+    console.log("coord",coordinates);
     return (
       <div className="maps-component">
-        <Maps center={coordinates} gameId={this.state.gameId} />
+        <Maps center={coordinates} gameId={this.state.gameId} objects={this.state.objectsInGame} />
       </div>
     );
   }
--- webapp/app/Maps.jsx
+++ webapp/app/Maps.jsx
@@ -6,29 +6,17 @@ const API_KEY = 'AIzaSyC2CY6tmBR88gS6cw_v6hf7JM2CSKz6ZgA';
 export default class Maps extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      objects: new Array(),
-    }
-  }
-  componentWillReceiveProps(nextProps) {
-    console.log("Maps Component - componentWillReceiveProps, gameId:", nextProps.gameId, "location:", nextProps.location);
-    var that = this;
-    fetch(`http://islandofthedead.com/game/${nextProps.gameId}`)
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(jsonResponse) {
-      that.setState({
-        objects: jsonResponse.occupants
-      });
-    });
   }
   renderObject(object) {
     return (
       <Occupant lat={object.latitude} lng={object.longitude} type={object.type} />
     );
   }
+  componentWillReceiveProps(nextProps) {
+    console.log("nextProps", nextProps);
+  }
   render() {
+    console.log("Maps", this.props.location);
     return (
        <GoogleMap
          bootstrapURLKeys={{
@@ -36,10 +24,9 @@ export default class Maps extends React.Component {
            language: 'nl'
          }}
         center={this.props.location}
-        defaultCenter={this.props.defaultCenter}
         defaultZoom={this.props.zoom}>
 
-        {this.state.objects.map(this.renderObject)}
+        {this.props.objects.map(this.renderObject)}
 
       </GoogleMap>
     );
@@ -47,8 +34,8 @@ export default class Maps extends React.Component {
 }
 
 Maps.defaultProps = {
+  objects: null,
   gameId: null,
   location: {lat: 52.373486, lng: 5.637864},
-  defaultCenter: {lat: 52.373486, lng: 5.637864},
   zoom: 18,
 };
